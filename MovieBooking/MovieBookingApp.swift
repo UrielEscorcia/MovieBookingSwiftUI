93cf1d94344d3c96c99fe71e0635548301b9c7b6
Uriel Escorcia <uris_ec@hotmail.com>
2024-12-11 10:07:25 -0600

    Initial Commit

---
+++ MovieBooking/MovieBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookingApp.swift
+//  MovieBooking
+//
+//  Created by Uriel  on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
